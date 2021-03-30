301351a81377694296f644dcb131eb39f2e37f5c
HynwonLEE <dl102030@naver.com>
2021-03-30 14:22:08 +0200

    이현원 3월 25일 제출

---
+++ lee0215/01. 25 Mar/Practice_2231/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_2231
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num_input = Convert.ToInt32(Console.ReadLine());
+            int n = num_input.ToString().Length;
+            int max = n * 9;
+            int result = 0;
+
+            for (int i = num_input - max; i < num_input; i++)
+            {
+                int a = i;
+                if (i > 0)
+                {
+                    for (int j = 0; j < i.ToString().Length; j++)
+                    {
+                        a += int.Parse(i.ToString()[j].ToString());
+                    }
+
+                    if (a == num_input)
+                    {
+                        result = i;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(result);
+
+        }
+    }
+}
---
+++ lee0215/01. 25 Mar/Practice_7568/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_7568
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int int_N = Convert.ToInt32(Console.ReadLine());
+            int[,] array_int = new int[int_N, 2];
+            int[] result_cnt = new int[int_N];
+            int[] result = new int[int_N];
+            for (int i = 0; i < int_N; i++)
+            {
+                string[] str = Console.ReadLine().Split(' ');
+                for (int j = 0; j < 2; j++)
+                {
+
+                    array_int[i, j] = Convert.ToInt32(str[j]);
+                }
+            }
+
+            for (int i = 0; i < int_N; i++)
+            {
+                result[i]++;
+                for (int z = 0; z < int_N; z++)
+                {
+
+                    if ((array_int[i, 0] < array_int[z, 0]) && array_int[i, 1] < array_int[z, 1])
+                    {
+                        result[i]++;
+                    }
+
+                }
+
+            }
+
+            for (int i = 0; i < int_N; i++)
+            {
+                Console.Write(result[i]);
+
+                Console.Write(' ');
+            }
+        }
+    }
+}
